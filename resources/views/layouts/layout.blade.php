ee317af93c4507ae3896fb5de0b09af541a5beef
Jawad Ashraf <jawadashraf78@gmail.com>
2025-05-09 15:00:10 +0100

    Tawk.to integrated

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -76,6 +76,18 @@
     </div>
 
     <x-scripts/>
-
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/681e09eac66711190f8d750a/1iqqlderj';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
   </body>
 </html>
